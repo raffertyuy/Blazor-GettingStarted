c140ee12b8288f484839d25ac4d0ac9a7facafe0
Rafferty Uy <rafferty.d.uy@outlook.com>
2020-05-21 08:12:37 +0000

    Completed tutorials in https://docs.microsoft.com/en-us/aspnet/core/tutorials/build-your-first-blazor-app?view=aspnetcore-3.1

---
+++ BlazorServer-GettingStarted/Shared/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
